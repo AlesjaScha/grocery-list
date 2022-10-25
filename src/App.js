7b046edd5b5624ed8fb5134f7b2d7ec3ec12aaca
AlesjaScha <alesja310780@gmail.com>
2022-10-25 23:22:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import image from './shopping.jpg';
+import imageTwo from './man.jpg';
+import { CroceryList } from './CroceryList';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='container'>
+      <img src={ image }width="200px" alt="shopping"/>
+      </div>
+      <div className='container'>
+      <h1>Grocery List</h1>
+      </div>
+
+      <CroceryList/>
+      <div className='container'>
+      <img src={imageTwo}width="200px"alt="man"/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
